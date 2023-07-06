ac0cfead8bb1b8527b856cbdb23ed5eceb2e35dc
CodeWithMakersMind <SoftwareTeam@makersmind.in>
2023-07-06 14:16:16 +0530

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> to save and reload the app.
+          Edit <code>src/App.js</code> to save and reload.
         </p>
         <a
           className="App-link"
