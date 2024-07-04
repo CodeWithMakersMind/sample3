a1c757715db19cd6b8e87f833285714f642ca197
CodeWithMakersMind <110817840+CodeWithMakersMind@users.noreply.github.com>
2024-07-04 16:08:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> testing
+          Edit <code>src/App.js</code> ReactApp
         </p>
         <a
           className="App-link"
